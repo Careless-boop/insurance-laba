20f081cb19520288e0da2dadba3665640aca4708
Careless-boop <vityahalushka@gmail.com>
2024-05-15 16:58:21 +0200

    Added section with vue js

---
+++ js/products.js
@@ -0,0 +1,95 @@
+const { createApp, onMounted, ref, computed, watch } = Vue;
+
+const appConfig = {
+  setup() {
+    const products = ref([
+      {
+        id: 1,
+        name: "Auto",
+        desc: "Average savings of over $750*",
+        img: "auto.svg",
+      },
+      {
+        id: 2,
+        name: "Homeowners",
+        desc: "Protection for you and the roof over your head",
+        img: "home.svg",
+      },
+      {
+        id: 3,
+        name: "Commercial Auto",
+        desc: "Customize your coverage to fit your business needs",
+        img: "commercial.svg",
+      },
+      {
+        id: 4,
+        name: "Boat",
+        desc: "Coverage for your days on the water",
+        img: "boat.svg",
+      },
+      {
+        id: 5,
+        name: "Motorcycle",
+        desc: "Coverage for life on the open road",
+        img: "motorcycle.svg",
+      },
+    ]);
+
+    const selectedProducts = ref([]);
+
+    const isPopupOpen = ref(false);
+
+    const addToCart = (product) => {
+      if (selectedProducts.value.some((item) => item.id === product.id)) {
+        alert("You have already choosed this product!");
+      } else {
+        selectedProducts.value.push(product);
+        localStorage.setItem(
+          "selectedProducts",
+          JSON.stringify(selectedProducts.value)
+        );
+      }
+    };
+
+    const removeFromCart = (index) => {
+      selectedProducts.value.splice(index, 1);
+      localStorage.setItem(
+        "selectedProducts",
+        JSON.stringify(selectedProducts.value)
+      );
+    };
+
+    const selectedProductsDetails = computed(() => {
+      return selectedProducts.value;
+    });
+
+    const selectedProductsCount = computed(() => {
+      return selectedProducts.value.length;
+    });
+
+    watch(selectedProductsCount, (newCount) => {
+      if (newCount === 0) {
+        isPopupOpen.value = false;
+      }
+    });
+
+    onMounted(() => {
+      console.log("Vue is mounted");
+      const storedProducts = localStorage.getItem("selectedProducts");
+      if (storedProducts) {
+        selectedProducts.value = JSON.parse(storedProducts);
+      }
+    });
+
+    return {
+      products,
+      selectedProductsDetails,
+      selectedProductsCount,
+      addToCart,
+      removeFromCart,
+      isPopupOpen,
+    };
+  },
+};
+
+createApp(appConfig).mount("#products");
